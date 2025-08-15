ee71da73a00d4b4886dd649b2cdded28374eaf98
Matheus Cassiano <144853588+matheuscassiano07@users.noreply.github.com>
2025-08-15 17:30:31 -0300

    api integrada

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
-document.query
+        document.addEventListener('DOMContentLoaded', () => {
+            const submitButton = document.getElementById('submitButton');
+            const cidadeInput = document.getElementById('cidadeInput');
+            const phoneInput = document.getElementById('phoneInput');
+            const alertModal = document.getElementById('alertModal');
+            const closeModalBtn = document.getElementById('closeModalBtn');
+            const modalMessage = document.getElementById('modalMessage');
+
+
+           
+            function showModal(message) {
+                modalMessage.textContent = message; 
+                alertModal.classList.add('visible');
+            }
+
+   
+            function hideModal() {
+                alertModal.classList.remove('visible');
+            }
+
+            submitButton.addEventListener('click',async (event) => {
+                event.preventDefault();
+
+                const cidadeValue = document.querySelector('#cidadeInput').value;
+                const phoneValue = document.querySelector('#phoneInput').value;
+               
+                if (cidadeValue === '' && phoneValue === '') {
+                    showModal('Insira a cidade e o Celular.');
+                }
+                 else if (cidadeValue === '') {
+                    showModal('Insira a cidade.');
+                } 
+                else if (phoneValue === '') {
+                    showModal('Insira seu número de celular.');
+                } 
+                else {
+                    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cidadeValue}&appid=49b232b0509cc66eded6ee0411f64892&units-metric&lang-pt_br`
+                    const results = await fetch(apiURL);
+                    const json = await results.json();
+                    console.log(json);
+                    
+                    if(json.cod == 200){
+                        showModal('Tudo certo! Agora fique de olho nas suas notificações para receber os futuros alertas!');
+                        
+
+
+                    }else{
+                        showModal('Cidade não encontrada.');
+                    }
+
+                }
+            });
+
+            function mostrarInfos(json){
+
+            }
+
+
+            closeModalBtn.addEventListener('click', hideModal);
+    
+            alertModal.addEventListener('click', (event) => {
+                if (event.target === alertModal) {
+                    hideModal();
+                }
+            });
+        });
